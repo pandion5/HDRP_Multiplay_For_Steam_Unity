1203c43a10e0fcb645fb753b47b143ff66df0612
Shifty <note3352@gmail.com>
2022-05-06 12:54:31 +0900

    오른손에 레이져 발싸

---
+++ VRMultiplayTemplate(HDRP)/Assets/MyWorkSpace/Scripts/Pointer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    public float m_DefaultLength = 5.0f;
+    public GameObject m_Dot;
+    public VRInputModule m_InputModule;
+
+    private LineRenderer m_LineRenderer = null;
+
+    void Awake()
+    {
+        m_LineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        UpdateLine();
+    }
+
+    private void UpdateLine()
+    {
+        float targetLength = m_DefaultLength;
+        RaycastHit hit = CreateRaycast(targetLength);
+        Vector3 endPosition = transform.position + (transform.forward * targetLength);
+        if(hit.collider != null)
+            endPosition = hit.point;
+
+        m_Dot.transform.position = endPosition;
+
+        m_LineRenderer.SetPosition(0, transform.position);
+        m_LineRenderer.SetPosition(1, endPosition);
+    }
+
+    private RaycastHit CreateRaycast(float length)
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        Physics.Raycast(ray, out hit, m_DefaultLength);
+
+        return hit;
+    }
+}
