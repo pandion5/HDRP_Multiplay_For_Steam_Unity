56738833e8f78d57495601b3ea491909e83282c3
Shifty <note3352@gmail.com>
2022-05-06 18:51:26 +0900

    준영이형 향기 장치 코드 추가

---
+++ VRMultiplayTemplate(HDRP)/Assets/MyWorkSpace/Scripts/DemoArduino.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class DemoArduino : MonoBehaviour
+{
+
+    public enum PortNumber
+    {
+        COM1, COM2, COM3, COM4,
+        COM5, COM6, COM7, COM8,
+        COM9, COM10, COM11, COM12,
+        COM13, COM14, COM15, COM16
+    }
+
+    private SerialPort serial;
+    private string fanvalue;
+    [SerializeField]
+    private PortNumber portNumber = PortNumber.COM5;
+    [SerializeField]
+    private string baudRate = "9600";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        serial = new SerialPort(portNumber.ToString(), int.Parse(baudRate));
+        if (!serial.IsOpen)
+        {
+            serial.Open();
+        }
+    }
+
+    private void Update()
+    {
+        if (!serial.IsOpen)
+        {
+            serial.Open();
+        }
+    }
+
+    // Update is called once per frame
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Tree")) // 唱公氢
+        {
+            Debug.Log("面倒");
+            fanvalue = "4";
+            serial.Write(fanvalue);
+            /*            servoValue = "u";
+                        serial.Write(servoValue);*/
+        }
+        if (other.gameObject.CompareTag("Flower")) // 采氢
+        {
+            fanvalue = "4";
+            serial.Write(fanvalue);
+            /*            servoValue = "u";
+                        serial.Write(servoValue);*/
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Tree")) //唱公氢
+        {
+            Debug.Log("面倒 场");
+            fanvalue = "5";
+            serial.Write(fanvalue);
+        }
+        if (other.gameObject.CompareTag("Flower")) //唱公氢
+        {
+            Debug.Log("面倒 场");
+            fanvalue = "5";
+            serial.Write(fanvalue);
+        }
+    }
+}
